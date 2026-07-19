16092f4795e3514cf97c5e63eb4e0ca5be6690ef
Fadel <fjachuf@gmail.com>
2026-07-18 22:23:28 -0300

    fix: subtitle overflow

--- src/components/MNavigationDrawer.vue
+++ src/components/MNavigationDrawer.vue
@@ -163,7 +163,7 @@ watch(
             </h2>
           </div>
 
-          <div class="flex flex-1 flex-col overflow-y-auto px-3 py-2">
+          <div class="flex min-h-0 flex-1 flex-col overflow-y-auto px-3 py-2">
             <template v-for="(section, si) in sections" :key="si">
               <div v-if="si > 0" class="my-1 border-t border-outline-variant" />
 
@@ -299,7 +299,7 @@ watch(
       </div>
 
       <div
-        class="flex flex-1 flex-col gap-1 overflow-y-auto overflow-x-hidden px-3 py-2"
+        class="flex min-h-0 flex-1 flex-col gap-1 overflow-y-auto overflow-x-hidden px-3 py-2"
       >
         <template v-for="(section, si) in sections" :key="si">
           <div v-if="si > 0" class="my-1 border-t border-outline-variant" />
